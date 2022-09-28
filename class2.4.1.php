156e093da07780aeaf08cae105989dce483c4ab4
AvramchikE <114004436+AvramchikE@users.noreply.github.com>
2022-09-28 22:41:27 +0300

    Delete class2.4.1.php

--- class2.4.1.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$time = DateTime::createFromFormat("Y-m-d", '2022-09-12'); 
-$date = DateTime::createFromFormat("Y-m-d", '2001-06-07'); 
-$interval = $time->diff($date); 
-echo "Разница: ";
-echo $interval->y, " год, "; 
-echo $interval->m, " месяца, "; 
-echo $interval->d, " дней."; 
