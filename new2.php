f6149458ae02469ff1d1d2131c23ca8d49f44d73
AvramchikE <114004436+AvramchikE@users.noreply.github.com>
2022-09-28 22:44:14 +0300

    Delete new2.php

--- new2.php
+++
@@ -1,9 +0,0 @@
-<?php
-$av = array();
-if (($file = fopen('data.csv', 'r'))) {
-$header = fgetcsv($file);
-while ($row = fgetcsv($file)) {
-  $av[] = array_combine($header, $row);
-}
-}
-print_r($av);
